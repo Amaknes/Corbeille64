a63ceae0b2e557f6df17ba4b2e629c384e70fdd2
Vincent <Vincent@10.144.50.35>
2017-06-01 14:37:54 +0200

    dfghjklmù*

--- src/dogfightview/DogfightView.java
+++ src/dogfightview/DogfightView.java
@@ -1,15 +1,47 @@
 package dogfightview;
 
-public class DogfightView {
+import gameframe.GameFrame;
+
+public class DogfightView implements Runnable, IViewSystem {
 	//=========ATTRIBUTES===========//
 	private EventPerformer eventPerformer;
+	private GameFrame gameFrame;
+	private GraphicsBuilder graphicsBuilder;
+	
+	//=========CONSTRUCTOR========//
+	  public DogfightView() {
+	eventPerformer = new EventPerformer();
+	gameFrame = new GameFrame();
+	graphicsBuilder = new GraphicsBuilder();
+	}
 
-	  //=========METHODS============//
+
+	//=========METHODS============//
 	public EventPerformer getEventPerformer() {
 		return eventPerformer;
 	}
 
 	public void setEventPerformer(EventPerformer eventPerformer) {
 		this.eventPerformer = eventPerformer;
+	}
+
+	@Override
+	public void run() {
+		
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String displayMessage(String message) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public void closeAll() {
+		// TODO Auto-generated method stub
+		
 	}
 }
