71ec9ab7ab204db240d85ff107b016d90e2a2c3a
Amaknes <amaury.vincent@viacesi.fr>
2017-06-02 14:09:16 +0200

    FinAjouts

--- src/dogfight/Controller/DogFightController.java
+++ src/dogfight/Controller/DogFightController.java
@@ -3,5 +3,13 @@ package dogfight.Controller;
 public class DogFightController {
 
 	private final int TIME_SLEEP = 30;
+	DogFightController(DogFightModel Model)
+	{
+		
+	}
+	public void Play()
+	{
+		
+	}
 
 }
